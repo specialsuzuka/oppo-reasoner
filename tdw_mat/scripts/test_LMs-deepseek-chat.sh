62331ec0f77ee641daf32dfeaf14097d4ca504c0
specialsuzuka <2391604943@qq.com>
2025-07-10 17:02:00 +0800

    new logger for coela messages

--- tdw_mat/scripts/test_LMs-deepseek-chat.sh
+++ tdw_mat/scripts/test_LMs-deepseek-chat.sh
@@ -3,7 +3,7 @@ port=10008
 pkill -f -9 "port $port"
 
 python3 tdw-gym/challenge_oppo.py \
---output_dir results \
+--output_dir results_coela \
 --lm_id $lm_id \
 --experiment_name LMs-$lm_id \
 --run_id run_1 \
@@ -11,10 +11,9 @@ python3 tdw-gym/challenge_oppo.py \
 --agents lm_agent lm_agent \
 --communication \
 --prompt_template_path LLM/prompt_com.csv \
---max_tokens 512 \
+--max_tokens 256 \
 --data_prefix dataset/dataset_test/ \
---eval_episodes 0 1 2 3 4 \
+--eval_episodes 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 \
 --screen_size 256 \
---no_save_img
 
 pkill -f -9 "port $port"
